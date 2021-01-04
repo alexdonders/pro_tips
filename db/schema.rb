9c927dcfa0e1ef1dc3b6544ea902e776803e1583
Ubuntu <ubuntu@ip-172-31-38-245.us-east-2.compute.internal>
2021-01-04 19:39:51 +0000

    first commit on cloud9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201222121110) do
+ActiveRecord::Schema.define(version: 20201221135926) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20201222121110) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.string "role"
+    t.string "role", default: "registered"
     t.string "avatar"
   end
 
